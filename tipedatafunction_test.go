89967fd492ad3f530d11897285734c2f2b26ef8d
nabilulilalbab <nabilulilalbab78@gmail.com>
2025-01-10 15:05:05 +0700

    commit 2

--- tipedatafunction_test.go
+++ tipedatafunction_test.go
@@ -5,28 +5,36 @@ import (
 	"testing"
 )
 
-type Greeter func (name string) 
-
+type Greeter func(name string)
 
 // cara 1
-func (g Greeter) sayHello (name string) {
-  g(name)
+func (g Greeter) sayHello(name string) {
+	g(name)
 }
 
-// cara 2 
-func (g Greeter) sayHello2(name string)  {
-  fmt.Println("hello, ",name )
+// cara 2
+func (g Greeter) sayHello2(name string) {
+	fmt.Println("hello, ", name)
 }
 
-func TestFunctionTipeTrick(t *testing.T)  {
-  greeter1 := Greeter(func(name string) {
-    fmt.Println("Halo", name, "apa kabar?")
-  }) 
-  greeter2 := Greeter(func(name string)  {
-    fmt.Println("selamat Pagi, ", name, "!")
-  })
-  greeter1.sayHello("nabil")
-  greeter2.sayHello("nabiel")
-  greeter1.sayHello2("nabiel")
-  greeter2.sayHello2("nabiel")
+func TestFunctionTipeTrick(t *testing.T) {
+	greeter1 := Greeter(func(name string) {
+		fmt.Println("Halo", name, "apa kabar?")
+	})
+	greeter2 := Greeter(func(name string) {
+		fmt.Println("selamat Pagi, ", name, "!")
+	})
+	greeter1.sayHello("nabil")
+	greeter2.sayHello("nabiel")
+	greeter1.sayHello2("nabiel")
+	greeter2.sayHello2("nabiel")
+}
+
+// penjelasan g name : g(name) mewakili anonymouse function jadi agar ketika mengirim function otomatis terpanggil contoh :
+
+func TestFunctionTipeTrick2(t *testing.T) {
+	// Ketika g(name) dipanggil, sebenarnya yang dijalankan adalah:
+	func(name string) {
+		fmt.Println("Halo,", name)
+	}("Budi")
 }
